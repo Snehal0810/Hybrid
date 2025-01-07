57fbb8745902bc94a1d8eb10bfe45452cad91525
sneha <sneha@LAPTOP-CBFKSMD2>
2025-01-07 19:02:40 +0530

    hiii 2nd commit

---
+++ src/test/java/Runner/Fblog.java
@@ -0,0 +1,27 @@
+package Runner;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class Fblog 
+{
+	public WebDriver driver;
+	@Test
+	public void log()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		
+	    driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+	}
+
+}
